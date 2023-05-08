20e6fa01ebf73f398fafd056cddadc4ae21ceb2f
sudirboyy <sudirboyy@gmail.com>
2023-05-08 15:31:26 +0700

    13

--- Friendclass/Friendclass.cpp
+++ Friendclass/Friendclass.cpp
@@ -23,3 +23,11 @@ void siswa::displayAll(orang& a) {
     cout << id << endl << a.nama;
 }
 
+void siswa::setId(int pId) {
+    id = pId;
+}  
+
+void orang::setNama(string pNama) {
+    nama = pNama;
+}
+
