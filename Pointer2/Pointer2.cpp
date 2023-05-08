8141413c1241b7d2a23665249c51364fba257452
sudirboyy <sudirboyy@gmail.com>
2023-05-08 15:13:56 +0700

    9

--- Pointer2/Pointer2.cpp
+++ Pointer2/Pointer2.cpp
@@ -1,7 +1,13 @@
-// Pointer2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
 
 int main()
 {
