f228a121a6ed0b9a3caf4b183587c870b8219e12
sudirboyy <sudirboyy@gmail.com>
2023-05-08 15:43:47 +0700

    14

--- Friendclass2/Friendclass2.cpp
+++ Friendclass2/Friendclass2.cpp
@@ -1,8 +1,13 @@
-// Friendclass2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
+class mahasiswa {
+private:
+    string nama;
+public:
+    friend void setNama(mahasiswa& a, string);
+};
 int main()
 {
     std::cout << "Hello World!\n";
