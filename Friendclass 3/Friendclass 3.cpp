1a72caaec2cfa16f9ee03b0cf1f45b63bec3051b
sudirboyy <sudirboyy@gmail.com>
2023-05-08 15:52:58 +0700

    16

--- Friendclass 3/Friendclass 3.cpp
+++ Friendclass 3/Friendclass 3.cpp
@@ -1,7 +1,6 @@
-// Friendclass 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std; 
 
 int main()
 {
