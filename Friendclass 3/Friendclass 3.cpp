c8c39675672b2d58a78443d4761b82432b1a4da5
sudirboyy <sudirboyy@gmail.com>
2023-05-08 15:55:35 +0700

    17

--- Friendclass 3/Friendclass 3.cpp
+++ Friendclass 3/Friendclass 3.cpp
@@ -2,6 +2,22 @@
 #include <string>
 using namespace std; 
 
+class pelajar;
+class manusia {
+public:
+    void showNilaiPelajar(pelajar&);
+
+};
+class pelajar {
+private:
+    int nilai;
+public:
+    pelajar() {
+        nilai = 100;
+    }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
